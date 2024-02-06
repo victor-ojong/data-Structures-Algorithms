3303c39e19d17235fd78dea2de140cc7ebd37a06
Victor Ojong <victorojong78@gmail.com>
2024-02-06 22:29:27 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,180 @@
+// const listOfNumbers = [90, 45, 7, 7, 400]
+
+// const calcMaxNumber = arr => {
+//    let  maXNumber = arr.at(0)
+//   for (let i = 0; i < listOfNumbers.length; i++) {
+   
+//     if (arr[i] > maXNumber) {
+//       maXNumber = arr[i]
+//     }
+//   }
+//   return maXNumber
+// }
+
+
+
+// console.log(calcMaxNumber(listOfNumbers))
+
+// const name = true ? 'victor': "Adedamola"
+
+// const newMethond = listOfNumbers.sort((a, b) => b - a)[(listOfNumbers.length-1)]
+
+// console.log(newMethond)
+
+
+// // Palidrone read backward as forward
+
+// const isPalidrome = (str) => {
+
+//   str =   str.split(' ').join('')
+//   const reversedString = str.split('').reverse().join('')
+
+//   return str === reversedString
+// }
+
+// console.log(isPalidrome('rise  to  vote sir'))
+
+// // anagram a string that has characters when sorted result to the same letter
+
+// const stringSorter = (text) => {
+  
+//   return text.split('').sort().join('')
+
+// }
+// const isAnagram = (str1, str2) => {
+
+//   const sortedString1 = stringSorter(str1)
+//   const sortedString2 = stringSorter(str2)
+//   console.log(sortedString1)
+//   console.log(sortedString2)
+
+//   return sortedString1 === sortedString2
+// }
+
+// console.log(isAnagram('isiii77vict', 'iiiis77vict'))
+
+// const memo = ['memo', 'memo', 'memo','memo','memo','memo','memo','memo','memo']
+
+// const large = new Array(70).fill('memo')
+
+// function findMemo(arr) {
+
+//   const t0 = performance.now()
+//   for (let i = 0; i<arr.length; i++){
+//     if (arr[0] === 'memo') {
+//       console.log('Found Memo!')
+
+//       break
+//     }
+//   }
+
+//   console.log(arr[10])
+//   const t1 = performance.now()
+
+//   return (`Hello Victor this process took ${t1-t0} milliseconds`)
+// }
+
+// console.log(findMemo(large))
+
+
+// Matching arrays in the between two arrays
+
+
+// function containsSameElements(arr1, arr2){
+
+//   for (let i = 0; i < arr1.length; i++){
+//     for (let j = 0; j < arr2.length; j++){
+
+//       if (arr1[i] === arr2[j]) {
+//         return true;
+//       }
+
+//     }
+//   }
+//   return false;
+// }
+
+// console.log(containsSameElements(array1, array2))
+
+
+
+// const matchingSameValues = (array1, array2) => {
+
+//   // using hash maps - objects
+
+//   let sortedObjects = {}
+
+//   for (let i = 0; i < array1.length; i++){
+//     if (!sortedObjects[array1[i]]) {
+//       sortedObjects[array1[i]] = true
+//     }
+//   }
+  
+
+
+//   for (let j = 0; j < array2.length; j++){
+  
+//   if (sortedObjects[array2[j]]) {
+//      return true
+//   }
+//   }
+//    return false
+//   }
+
+// console.log(matchingSameValues(array1, array2))
+
+
+
+// const reverseSortedArrays = (arr1, arr2) =>arr1.concat(arr2).sort((a,b)=> a-b )
+
+// console.log(reverseSortedArrays(array1, array2))
+
+
+const array1 = [1, 2, 3, 4, 6, 2]
+const array2 = [8, 9, 0, 10, 6, 7]
+
+
+const sumOfNumbersAndThierIndexes = (arr1, arr2, target) => {
+
+
+  // error handling 
+  if (arr1.length < 1 || arr2.length < 1 || ((typeof arr1) !== 'object')) {
+    
+    return 'Invalid Inputs'
+    
+  }
+
+  const firstSetObject = {}
+  
+  let arrayOfCombinators = []
+
+  //  create a hash table of the first array to avoid nested arrays TC of 0(n^2)
+  for (let i = 0; i < arr1.length; i++){
+
+    if (!firstSetObject[arr1[i]]) {
+
+      firstSetObject[arr1[i]] = arr1[i]
+      
+    }
+
+  }
+
+  //O(a)
+
+
+  for (let j = 0; j < arr2.length; j++){
+
+    if ((firstSetObject[arr1[j]] + arr2[j]) === target) {
+      
+     arrayOfCombinators = arrayOfCombinators.concat([arr1.indexOf(arr1[j]) , arr2.indexOf(arr2[j]) ])
+
+    }
+  }
+  //O(b)
+  
+  return arrayOfCombinators
+}
+
+console.log(sumOfIndex(array1, array2, 9)) 
+
+// O(a+b)  Time complexity
